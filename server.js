8a0255fef1a5fc3c2b4c02fc2b29ff9d3c3cabd4
Ezrabro <67427897+Ezrabro@users.noreply.github.com>
2021-12-30 15:00:07 +0330

    server fiexd

--- server.js
+++ server.js
@@ -12,80 +12,6 @@ app.use(function(req, res, next) {
     );
     next();
 });
-app.get('/newUser', function(req, res){
-    var prename = req.query.prename;
-    var lastname = req.query.lastname;
-    //check if prename is empty
-    if(prename == '' || lastname == '' || prename == undefined || lastname == undefined || prename == null || lastname == null){
-        res.json('Please enter a name');
-    }
-    else{
-        if(users.length == 0 || users.length == 1){
-            res.json('no user');
-        }
-        else{
-            var randomUser = Math.floor(Math.random() * users.length);
-            while(prename == users[randomUser] || lastname == users[randomUser]){
-                randomUser = Math.floor(Math.random() * users.length);
-            }
-            //send the new user and the random user
-            res.json(users[randomUser]);
-        }
-    }
-});
-app.get('/addUser', function(req, res){
-    var prename = req.query.prename;
-    //check if prename is empty
-    if(prename == ''){
-        res.json('Please enter a name');
-    }
-    else{
-        //check if user already exists
-        if(users.indexOf(prename) == -1){
-            users.push(prename);
-            res.json('User added');
-        }
-        else{
-            var json = {
-                'error': 'User already exists'
-            };
-            res.json(json);
-        }
-    }
-});
-app.get('/getUsers', function(req, res){
-    var json = {
-        'status': 'ok',
-        'users': users
-    };
-    res.json(users);
-});
-app.get('/deleteUser', function(req, res){
-    var prename = req.query.prename;
-    if(prename == '' || prename == undefined || prename == null){
-        var json = {
-            'error': 'Please enter a name'
-        };
-        res.json(json);
-    }
-    else{
-        if(users.indexOf(prename) == -1){
-            var json = {
-                'status': 'OK',
-                'message': 'User does not exist'
-            };
-            res.json(json);
-        }
-        else{
-            users.splice(users.indexOf(prename), 1);
-            var json = {
-                'status': 'OK',
-                'message': 'User deleted'
-            };
-            res.json(json);
-        }
-    }
-});
 app.get('/deleteAllUsers', function(req, res){
     users = [];
     res.json('Users deleted');
@@ -96,13 +22,106 @@ app.get('/', function(req, res){
         'name': 'Amegol Server',
         'version': '1.0.0',
         'commands': [
-            '/newUser',
-            '/addUser',
+            '/joinLobby?name=<name>',
+            '/leaveLobby?name=<name>',
+            '/deleteAllUsers',
             '/getUsers',
-            '/deleteUser'
+            '/requestUser?name=<name>&previousUser=<previousUser>',
         ],
         'description': 'This is a server for the Amegol',
         'author': 'Ezrabro',
     };
     res.json(data);
+});
+app.get('/joinLobby', function(req, res){
+    var thisUser = req.query.user;
+    if(thisUser == '' || thisUser == undefined || thisUser == null){
+        var json = {
+            'error': 'Please enter a name'
+        };
+        res.json(json);
+    }
+    else{
+        if(users.indexOf(thisUser) == -1){
+            users.push(thisUser);
+            var json = {
+                'status': 'OK',
+                'message': 'User added'
+            };
+            res.json(json);
+        }
+        else{
+            var json = {
+                'error': 'User already exists'
+            };
+            res.json(json);
+        }
+    }
+});
+app.get('/requestUser', function(req, res){
+    var thisUser = req.query.user;
+    var previousUser = req.query.previousUser;
+    if(thisUser == '' || thisUser == undefined || thisUser == null){
+        var json = {
+            'error': 'Please enter a name'
+        };
+        res.json(json);
+    }
+    else{
+        //chose a random user
+        if(users.length == 1){
+            var json = {
+                'status': 'OK',
+                'message': 'You are the only user'
+            };
+            res.json(json);
+        }
+        else{
+            var randomUser = Math.floor(Math.random() * users.length);
+            while(thisUser == users[randomUser] || previousUser == users[randomUser]){
+                randomUser = Math.floor(Math.random() * users.length);
+            }
+            var json = {
+                'status': 'OK',
+                'message': users[randomUser]
+            };
+            res.json(json);
+            //remove both users
+            users.splice(users.indexOf(thisUser), 1);
+            users.splice(users.indexOf(previousUser), 1);
+        }
+    }
+});
+app.get('/leaveLobby', function(req, res){
+    var thisUser = req.query.user;
+    if(thisUser == '' || thisUser == undefined || thisUser == null){
+        var json = {
+            'error': 'Please enter a name'
+        };
+        res.json(json);
+    }
+    else{
+        if(users.indexOf(thisUser) == -1){
+            var json = {
+                'status': 'OK',
+                'message': 'User does not exist'
+            };
+            res.json(json);
+        }
+        else{
+            users.splice(users.indexOf(thisUser), 1);
+            var json = {
+                'status': 'OK',
+                'message': 'User deleted'
+            };
+            res.json(json);
+        }
+    }
+});
+app.get('/getUsers', function(req, res){
+    var json = {
+        'status': 'ok',
+        'users': users
+    };
+    res.json(json);
 });
